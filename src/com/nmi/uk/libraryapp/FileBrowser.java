35c96dfe9cf7d154947344373b0cdada55898fbe
Kerensky256 <kerensky1@btinternet.com>
2014-08-31 11:10:19 +0100

    Gui app

---
+++ src/com/nmi/uk/libraryapp/FileBrowser.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright (C) 2014 Darren
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package com.nmi.uk.libraryapp;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author Darren
+ */
+public class FileBrowser extends JFrame {
+  private JTextField filename = new JTextField(), dir = new JTextField();
+
+  private JButton open = new JButton("Open"), save = new JButton("Save");
+
+  public FileBrowser() {
+    JPanel p = new JPanel();
+    open.addActionListener(new OpenL());
+    p.add(open);
+    save.addActionListener(new SaveL());
+    p.add(save);
+    Container cp = getContentPane();
+    cp.add(p, BorderLayout.SOUTH);
+    dir.setEditable(false);
+    filename.setEditable(false);
+    p = new JPanel();
+    p.setLayout(new GridLayout(2, 1));
+    p.add(filename);
+    p.add(dir);
+    cp.add(p, BorderLayout.NORTH);
+  }
+
+  class OpenL implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+      JFileChooser c = new JFileChooser();
+      // Demonstrate "Open" dialog:
+      int rVal = c.showOpenDialog(FileBrowser.this);
+      if (rVal == JFileChooser.APPROVE_OPTION) {
+        filename.setText(c.getSelectedFile().getName());
+        dir.setText(c.getCurrentDirectory().toString());
+      }
+      if (rVal == JFileChooser.CANCEL_OPTION) {
+        filename.setText("You pressed cancel");
+        dir.setText("");
+      }
+    }
+  }
+
+  class SaveL implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+      JFileChooser c = new JFileChooser();
+      // Demonstrate "Save" dialog:
+      int rVal = c.showSaveDialog(FileBrowser.this);
+      if (rVal == JFileChooser.APPROVE_OPTION) {
+        filename.setText(c.getSelectedFile().getName());
+        dir.setText(c.getCurrentDirectory().toString());
+      }
+      if (rVal == JFileChooser.CANCEL_OPTION) {
+        filename.setText("You pressed cancel");
+        dir.setText("");
+      }
+    }
+  }
+
+  public static void run(JFrame frame, int width, int height) {
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setSize(width, height);
+    frame.setVisible(true);
+  }
+}
